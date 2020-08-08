ad2f31e49797ef4abf1f134ec8d86df1d3facf2a
Igor Bedesqui <igorbedesqui@gmail.com>
2020-08-08 20:06:33 -0300

    updated global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,32 @@
 import { createGlobalStyle } from "styled-components";
 
+const breakpoints = [48, 64, 144];
+
+export const mq = breakpoints.map((bp) => `@media (min-width: ${bp}rem)`);
+
 export default createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
+        outline: 0;
         box-sizing: border-box;
+    }
+
+    body {
+        -webkit-font-smoothing: antialiased !important;
+    }
+    body html #root {
+        height: 100%;
+    }
+
+    :root{
+        font-family: 'Roboto', sans-serif;
+        font-size: 60%;
+    }
+
+${mq[0]}{
+    :root{
+        font-size: 62.5%
+    }
     }
 `;
