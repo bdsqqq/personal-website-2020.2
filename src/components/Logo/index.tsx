f8b82b71a06b1f0ba5ced2a3695221f84a8c8357
Igor Bedesqui <igorbedesqui@gmail.com>
2020-08-08 20:09:38 -0300

    added styling

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { LogoType } from "./styles";
 
 const Logo = () => {
   return (
-    <span>
+    <LogoType>
       igor <br /> bedesqui
-    </span>
+    </LogoType>
   );
 };
 
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const LogoType = styled.span`
+  font-size: 2.4rem;
+  letter-spacing: 20%;
+  text-transform: uppercase;
+`;
