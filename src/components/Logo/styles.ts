d55a8db35ff7c7b24f2e7a2063639e1011c271d0
Igor Bedesqui <igorbedesqui@gmail.com>
2020-08-08 20:46:17 -0300

    added prop support and updated styles

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
 export const LogoType = styled.span`
+  color: ${(props) => props.color || props.theme.colors.primaryWhite};
+
   font-size: 2.4rem;
+  line-height: 2.8rem;
   letter-spacing: 20%;
   text-transform: uppercase;
 `;
