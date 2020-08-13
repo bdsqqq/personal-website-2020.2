3f13c40ead94acd27ace8dccba3020f6996ac2e3
Igor Bedesqui <igorbedesqui@gmail.com>
2020-08-13 18:25:32 -0300

    updated Logo styles. Added media Query

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,10 +1,16 @@
 import styled from "styled-components";
+import { mq } from "../../styles/global";
 
-export const LogoType = styled.span`
+export const LogoType = styled.p`
   color: ${(props) => props.color || props.theme.colors.primaryWhite};
 
-  font-size: 2.4rem;
-  line-height: 2.8rem;
+  font-size: 1.8rem;
+  line-height: 1.6rem;
   letter-spacing: 20%;
   text-transform: uppercase;
+
+  ${mq[1]} {
+    font-size: 2.4rem;
+    line-height: 2.8rem;
+  }
 `;
