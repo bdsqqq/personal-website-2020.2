b559a7c0ce21909359aea50f40d45be7c128176d
Igor Bedesqui <igorbedesqui@gmail.com>
2020-08-14 16:09:30 -0300

    Updated the Header tests

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -8,38 +8,44 @@ import GlobalStyles from "../styles/global";
 
 import DefaultTheme from "../styles/themes/default";
 
-afterEach(cleanup);
+describe("Header", () => {
+  afterEach(cleanup);
 
-it("renders IGOR BEDESQUI", () => {
-  const { getByText } = render(
-    <ThemeProvider theme={DefaultTheme}>
-      <GlobalStyles />
-      <Header />
-    </ThemeProvider>
-  );
-  const textElement = getByText(/IGOR BEDESQUI/i);
-  expect(textElement).toBeInTheDocument();
-});
+  it("renders IGOR BEDESQUI", () => {
+    const { getByText } = render(
+      <ThemeProvider theme={DefaultTheme}>
+        <GlobalStyles />
+        <Header />
+      </ThemeProvider>
+    );
+    const textElement = getByText(/IGOR BEDESQUI/i);
+    expect(textElement).toBeInTheDocument();
+  });
 
-it("renders the Menu Icon", () => {
-  const { getByLabelText } = render(
-    <ThemeProvider theme={DefaultTheme}>
-      <GlobalStyles />
-      <Header />
-    </ThemeProvider>
-  );
-  const svgElement = getByLabelText(/Abrir Menu/i);
-  expect(svgElement).toBeInTheDocument();
-});
+  it("renders the Menu Icon", () => {
+    const { getByLabelText } = render(
+      <ThemeProvider theme={DefaultTheme}>
+        <GlobalStyles />
+        <Header />
+      </ThemeProvider>
+    );
+    const svgElement = getByLabelText(/Abrir Menu/i);
+    expect(svgElement).toBeInTheDocument();
+  });
 
-it("renders the Close Icon when the user Clicks the Menu Icon", () => {
-  render(
-    <ThemeProvider theme={DefaultTheme}>
-      <GlobalStyles />
-      <Header />
-    </ThemeProvider>
-  );
-  fireEvent.click(screen.getByLabelText("Abrir Menu"));
+  it("Swaps the Icon between Open Menu and Close Menu when the user clicks on the Icon", () => {
+    render(
+      <ThemeProvider theme={DefaultTheme}>
+        <GlobalStyles />
+        <Header />
+      </ThemeProvider>
+    );
+    fireEvent.click(screen.getByLabelText("Abrir Menu"));
 
-  expect(screen.getByLabelText("Fechar Menu")).toBeInTheDocument();
+    expect(screen.getByLabelText("Fechar Menu")).toBeInTheDocument();
+
+    fireEvent.click(screen.getByLabelText("Fechar Menu"));
+
+    expect(screen.getByLabelText("Abrir Menu")).toBeInTheDocument();
+  });
 });
