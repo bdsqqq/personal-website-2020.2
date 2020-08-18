5b19ee62f78b82b08ce2cde82caf6510019da809
Igor Bedesqui <igorbedesqui@gmail.com>
2020-08-18 17:32:21 -0300

    updated Menu styles

--- src/components/Menu/styles.tsx
+++ src/components/Menu/styles.tsx
@@ -1,22 +1,27 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Overlay = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   z-index: 1000;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   opacity: 0.98;
   overflow-x: hidden;
   overflow-y: hidden;
+  padding: 2.4rem;
 
+  background: ${(props) => props.theme.colors.secondaryBlack};
+`;
+
+export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
-  background: ${(props) => props.theme.colors.secondaryBlack};
+  width: 100%;
+  height: 80%;
 `;
 
 export const Nav = styled.nav`
