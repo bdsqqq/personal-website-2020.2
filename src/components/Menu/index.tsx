0fdd38062b645b100f3fe038141a43f23551dead
Igor Bedesqui <igorbedesqui@gmail.com>
2020-08-18 18:04:59 -0300

    added reactDOM Portal and functionality

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import Header from "../Header";
 import { Overlay, Wrapper, Nav, MenuItem } from "./styles";
@@ -7,19 +8,23 @@ interface MenuProps {
   isOpen: boolean;
   hide: any;
 }
-const Menu: React.FC<MenuProps> = ({ isOpen, hide }) => {
-  return (
-    <Overlay onClick={hide}>
-      <Header isOpen={isOpen} toggleIsOpen={hide} />
-      <Wrapper>
-        <Nav>
-          <MenuItem href="#Sobre mim">Sobre mim</MenuItem>
-          <MenuItem href="#Folio">Folio</MenuItem>
-          <MenuItem href="#contato">Contato</MenuItem>
-        </Nav>
-      </Wrapper>
-    </Overlay>
-  );
-};
+const Menu: React.FC<MenuProps> = ({ isOpen, hide }) =>
+  isOpen
+    ? ReactDOM.createPortal(
+        <>
+          <Overlay onClick={hide}>
+            <Header isOpen={isOpen} toggleIsOpen={hide} />
+            <Wrapper>
+              <Nav>
+                <MenuItem href="#Sobre mim">Sobre mim</MenuItem>
+                <MenuItem href="#Folio">Folio</MenuItem>
+                <MenuItem href="#contato">Contato</MenuItem>
+              </Nav>
+            </Wrapper>
+          </Overlay>
+        </>,
+        document.body
+      )
+    : null;
 
 export default Menu;
