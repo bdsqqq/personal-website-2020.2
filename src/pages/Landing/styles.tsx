d8d73cb79c552eda18887719ad628dc4393e7e5f
Igor Bedesqui <igorbedesqui@gmail.com>
2020-08-20 17:29:59 -0300

    updated Landing styles

--- src/pages/Landing/styles.tsx
+++ src/pages/Landing/styles.tsx
@@ -4,16 +4,18 @@ import { mq } from "../../styles/global";
 export const Page = styled.div`
   position: relative;
   width: 100%;
-  background: ${(props) => props.theme.colors.secondaryBlack};
+  /*background: ${(props) => props.theme.colors.secondaryBlack};*/
 
-  padding: 2.4rem;
   overflow-x: hidden;
 `;
 
-export const Wrapper = styled.div`
+export const HeroWrapper = styled.div`
   position: relative;
   width: 100%;
-  min-height: 100vh;
+  height: 100vh;
+
+  padding: 2.4rem;
+  background: url("https://images.unsplash.com/photo-1560759995-d237fb70a79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1866&q=80");
 `;
 
 export const Main = styled.main`
@@ -21,12 +23,12 @@ export const Main = styled.main`
   flex-flow: column;
 `;
 
-export const Title = styled.h1`
-  font-weight: 500;
+export const HeroText = styled.h1`
+  font-weight: 700;
   font-size: 3.6rem;
-  letter-spacing: 0.95rem;
-  max-width: 60rem;
-  margin-top: 16rem;
+  letter-spacing: 0.2em;
+  max-width: 80rem;
+  margin-top: 18rem;
 
   color: ${(props) => props.theme.colors.primaryWhite};
 
@@ -35,7 +37,16 @@ export const Title = styled.h1`
   }
 `;
 
+export const HeroWord = styled.span`
+  font-size: 7.2rem;
+
+  ${mq[1]} {
+    font-size: 14.4rem;
+  }
+`;
+
 export const Footer = styled.footer`
+  padding: 2.4rem;
   position: relative;
   z-index: 1010;
 `;
