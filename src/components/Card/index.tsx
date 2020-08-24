86593367c2f909ce44e926f9f707c45de3dbdfae
Igor Bedesqui <igorbedesqui@gmail.com>
2020-08-24 20:16:17 -0300

    implemented Card animation

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
 
 import { Card, MetaCard, Thumb } from "./styles";
 
@@ -17,20 +18,48 @@ interface CardProps {
 }
 
 const CardComponent: React.FC<CardProps> = ({ project }) => {
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  const AnimatedMetaCard = animated(MetaCard);
+
+  const { scale, opacity } = useSpring({
+    config: {
+      velocity: 0,
+      tension: 280,
+      friction: 20,
+    },
+    from: { scale: "scale(0)", opacity: 0 },
+    to: {
+      scale: expanded ? "scale(1)" : "scale(0)",
+      opacity: expanded ? 1 : 0,
+    },
+  });
   return (
-    <Card>
+    <Card
+      onMouseEnter={() => {
+        setExpanded(true);
+      }}
+      onMouseLeave={() => {
+        setExpanded(false);
+      }}
+    >
       <Thumb
         backgroundImg={
           "https://images.unsplash.com/photo-1598281802472-51ee16bfa65c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=20"
         }
       />
-      <MetaCard>
+      <AnimatedMetaCard
+        style={{
+          transform: scale,
+          opacity: opacity,
+        }}
+      >
         <h1>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse facilis
           dolorum excepturi cupiditate tempore totam iure nobis impedit aliquid
           vero?
         </h1>
-      </MetaCard>
+      </AnimatedMetaCard>
     </Card>
   );
 };
