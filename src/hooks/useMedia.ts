3f1cb8a62f7241dac2255322fecf50aa3f4d6ac7
Igor Bedesqui <igorbedesqui@gmail.com>
2020-08-25 18:50:14 -0300

    changed useMedia from js to ts

--- src/hooks/useMedia.ts
+++ src/hooks/useMedia.ts
@@ -1,14 +1,15 @@
 //Made by @drmda
 import { useEffect, useState } from "react";
 
-export default function useMedia(queries, values, defaultValue) {
+export default function useMedia(queries: any, values: any, defaultValue: any) {
   const match = () =>
-    values[queries.findIndex((q) => matchMedia(q).matches)] || defaultValue;
+    values[queries.findIndex((q: any) => matchMedia(q).matches)] ||
+    defaultValue;
   const [value, set] = useState(match);
   useEffect(() => {
     const handler = () => set(match);
     window.addEventListener("resize", handler);
-    return () => window.removeEventListener(handler);
+    return () => window.removeEventListener("resize", handler);
   });
   return value;
 }
