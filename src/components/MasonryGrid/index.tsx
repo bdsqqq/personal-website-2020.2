78d6e66f4b9563a42a2066f2d28232af0de6b5aa
Igor Bedesqui <igorbedesqui@gmail.com>
2020-08-31 14:19:33 -0300

    updated columns useMedia hook, optmized animatons by removing width animation, added opacity animation and cleaned up comments

--- src/components/MasonryGrid/index.tsx
+++ src/components/MasonryGrid/index.tsx
@@ -2,24 +2,26 @@ import React, { useState } from "react";
 import data from "./data";
 import { useTransition, animated } from "react-spring";
 import useMedia from "../../hooks/useMedia";
+import usepureReverseArray from "../../hooks/usePureReverseArray";
 import useMeasure from "react-use-measure";
 
 import Card from "../Card";
 
 import { List } from "./styles";
+import { breakpoints } from "../../styles/global";
 
 const MasonryGrid = React.memo(() => {
-  // Hook1: Tie media queries to the number of columns
+  // The array is being reverted instead of the number of columns because when i do it the other way it gets stuck on the number of columnsin [0]
   const columns = useMedia(
-    ["(min-width: 1500px)", "(min-width: 1000px)", "(min-width: 600px)"],
+    usepureReverseArray(breakpoints.map((bp) => `(min-width: ${bp}rem)`)),
     [4, 3, 2],
     1
   );
-  // Hook2: Measure the width of the container element
-  const [bind, { width }] = useMeasure();
-  // Hook3: Hold items
+
+  const [measureRef, { width }] = useMeasure();
+
   const [items] = useState(data);
-  // Form a grid of stacked items using width & columns we got from hooks 1 & 2
+
   let heights = new Array(columns).fill(0); // Each column gets a height starting with zero
   let gridItems = items.map((child, i) => {
     const column = heights.indexOf(Math.min(...heights)); // Basic masonry-grid placing, puts tile into the smallest column using Math.min
@@ -29,47 +31,46 @@ const MasonryGrid = React.memo(() => {
     ]; // X = container width / number of columns * column index, Y = it's just the height of the current column
     return { ...child, xy, width: width / columns, height: child.height / 2 };
   });
-  // Hook4: Turn the static grid values into animated transitions, any addition, removal or change will be animated
+
   const transitions: any = useTransition(gridItems, (item) => item.css, {
-    from: ({ xy, width, height }) => ({ xy, width, height, opacity: 0 }),
-    enter: ({ xy, width, height }) => ({ xy, width, height, opacity: 1 }),
-    update: ({ xy, width, height }) => ({ xy, width, height }),
+    from: ({ xy, height }) => ({ xy, height, opacity: 0 }),
+    enter: ({ xy, height }) => ({ xy, height, opacity: 1 }),
+    update: ({ xy, height }) => ({ xy, height }),
     leave: { height: 0, opacity: 0 },
     config: { mass: 3, tension: 500, friction: 100 },
     trail: 15,
   });
-  // Render the grid
+
   return (
-    <List ref={bind} style={{ height: `${Math.max(...heights)}rem` }}>
-      {transitions.map(
-        ({ item, props: { xy, height, opacity, ...rest }, key }: any) => (
-          <animated.div
-            key={key}
-            style={{
-              height: `${item.height}rem`,
-              transform: xy.interpolate(
-                (x: number, y: number) => `translate3d(${x}px,${y}rem,0)`
-              ),
-              ...rest,
+    <List ref={measureRef} style={{ height: `${Math.max(...heights)}rem` }}>
+      {transitions.map(({ item, props: { xy, height, ...rest }, key }: any) => (
+        <animated.div
+          key={key}
+          style={{
+            height: `${item.height}rem`,
+            transform: xy.interpolate(
+              (x: number, y: number) => `translate3d(${x}px,${y}rem,0)`
+            ),
+            width: item.width,
+            ...rest,
+          }}
+        >
+          <Card
+            project={{
+              id: item.id,
+              ano: "",
+              demo: "",
+              img: item.css,
+              projeto: "",
+              role: "",
+              source: "",
+              tools: [""],
+              order: "1",
+              height: item.height,
             }}
-          >
-            <Card
-              project={{
-                id: item.id,
-                ano: "",
-                demo: "",
-                img: item.css,
-                projeto: "",
-                role: "",
-                source: "",
-                tools: [""],
-                order: "1",
-                height: item.height,
-              }}
-            />
-          </animated.div>
-        )
-      )}
+          />
+        </animated.div>
+      ))}
     </List>
   );
 });
