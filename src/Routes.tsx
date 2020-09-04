f761f4abbb0123f49d9cddbac926d36c56d50a75
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-03 22:48:21 -0300

    added Folio to Routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
+import Folio from "./pages/Folio";
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Landing} />
+        <Route exact path="/folio" component={Folio} />
       </Switch>
     </Router>
   );
