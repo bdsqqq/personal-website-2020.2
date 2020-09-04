ff04ef1a05dfdf833e688c129f3ff2aad0ff82ba
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-03 23:46:21 -0300

    updated Folio

--- src/pages/Folio/index.tsx
+++ src/pages/Folio/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+import Hero from "../../components/HeroWithHeader";
+import MasonryGrid from "../../components/MasonryGrid";
+import SocialIcons from "../../components/SocialIcons";
+
+import {
+  Page,
+  ContentSection,
+  SectionTitle,
+  Footer,
+} from "../../styles/general";
+
 const Folio = () => {
-  return <h1>Folio</h1>;
+  return (
+    <Page>
+      <Hero sm />
+
+      <ContentSection>
+        <SectionTitle>FOLIO</SectionTitle>
+        <MasonryGrid />
+      </ContentSection>
+
+      <Footer>
+        <SocialIcons color="dark" />
+      </Footer>
+    </Page>
+  );
 };
 
 export default Folio;
