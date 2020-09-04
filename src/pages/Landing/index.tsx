040f0b47deca87461c071ac902d6f7b612ccb5be
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-03 23:47:11 -0300

    updated Landing

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,42 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 
 import {
   Page,
-  HeroWrapper,
+  ContentSection,
+  SectionTitle,
+  Footer,
+} from "../../styles/general";
+
+import {
   Hero,
   Main,
   HeroText,
   HeroWord,
-  ContentSection,
   IgorWrapper,
   StyledIgorSVG,
   InseptionWrapper,
   StyledInseptionSVG,
-  SectionTitle,
-  Footer,
 } from "./styles";
 
-import useToggleIsOpen from "../../hooks/useToggleState";
-
-import Header from "../../components/Header";
-import MasonryGrid from "../../components/MasonryGrid";
+import HeroWrapper from "../../components/HeroWithHeader";
 import SocialIcons from "../../components/SocialIcons";
-import Menu from "../../components/Menu";
-
-// import Card from "../../components/Card";
 
 const Landing = () => {
-  const [menuIsOpen, setmenuIsOpen] = useState<boolean>(false);
-
-  const ToggleMenu = () => {
-    useToggleIsOpen(menuIsOpen, setmenuIsOpen);
-  };
-
   return (
     <>
       <Page>
         <HeroWrapper>
-          <Header color="light" isOpen={menuIsOpen} toggleIsOpen={ToggleMenu} />
           <Hero>
             <Main>
               <HeroText data-testid="MainTitleText">
@@ -54,57 +43,12 @@ const Landing = () => {
         </HeroWrapper>
 
         <ContentSection>
-          <SectionTitle>FOLIO...</SectionTitle>
-
-          <MasonryGrid />
-
-          {/*<Card
-            project={{
-              id: "test",
-              ano: "test",
-              demo: "test",
-              img:
-                "https://images.unsplash.com/photo-1597938430467-c7a5f65c24f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-              projeto: "test",
-              role: "test",
-              source: "test",
-              tools: ["test", "test"],
-              order: "test",
-            }}
-          />
-          <Card
-            project={{
-              id: "test",
-              ano: "test",
-              demo: "test",
-              img:
-                "https://images.unsplash.com/photo-1597688642705-ab966bf086fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-              projeto: "test",
-              role: "test",
-              tools: ["test", "test"],
-              order: "test",
-            }}
-          />
-          <Card
-            project={{
-              id: "test",
-              ano: "test",
-              demo: "test",
-              img:
-                "https://images.unsplash.com/photo-1597892672196-3bb8b5c06171?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-              projeto: "test",
-              role: "test",
-              source: "test",
-              tools: ["test", "test"],
-              order: "test",
-            }}
-          />*/}
+          <SectionTitle>Something Something</SectionTitle>
         </ContentSection>
 
         <Footer>
           <SocialIcons color="dark" />
         </Footer>
-        <Menu isOpen={menuIsOpen} hide={ToggleMenu} />
       </Page>
     </>
   );
--- src/pages/Landing/styles.tsx
+++ src/pages/Landing/styles.tsx
@@ -4,26 +4,6 @@ import { mq } from "../../styles/global";
 import IgorSVG from "../../components/IgorSVG";
 import InseptionSVG from "../../components/InseptionSVG";
 
-export const Page = styled.div`
-  position: relative;
-  width: 100%;
-  /*background: ${(props) => props.theme.colors.secondaryBlack};*/
-
-  overflow-x: hidden;
-`;
-
-export const HeroWrapper = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100vh;
-
-  padding: 2.4rem;
-  background-image: url("https://images.unsplash.com/photo-1560759995-d237fb70a79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1866&q=60");
-  background-position: center center;
-  background-repeat: no-repeat;
-  background-size: cover;
-`;
-
 export const Hero = styled.div`
   position: relative;
   display: flex;
@@ -113,30 +93,3 @@ export const InseptionWrapper = styled.div`
 `;
 
 export const StyledInseptionSVG = styled(InseptionSVG)``;
-
-export const ContentSection = styled.article`
-  position: relative;
-  margin-top: -8rem;
-  border-radius: 6rem 0 0 0;
-  min-height: 50vh;
-  padding: 6rem 2rem;
-
-  background: ${(props) => props.theme.colors.primaryWhite};
-  color: ${(props) => props.theme.colors.secondaryBlack};
-
-  ${mq[1]} {
-    margin-top: -4rem;
-    padding: 6rem 8rem;
-  }
-`;
-
-export const SectionTitle = styled.h2`
-  font-size: 4.8rem;
-  letter-spacing: 0.2em;
-`;
-
-export const Footer = styled.footer`
-  padding: 2.4rem;
-  position: relative;
-  z-index: 1010;
-`;
