9b78c6685ba8e0cdbc153d9d5ad7f4468a5e22cc
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-09 17:19:21 -0300

    updated Hero styling

--- src/components/HeroWithHeader/styles.tsx
+++ src/components/HeroWithHeader/styles.tsx
@@ -10,7 +10,7 @@ export const HeroWrapper = styled(Div)`
   position: relative;
   width: 100%;
   height: ${(props) => (props.sm ? "30vh" : "100vh")};
-  min-height: 48rem;
+  min-height: ${(props) => (props.sm ? "32rem" : "48rem")};
   padding: 2.4rem;
   background-image: url("https://images.unsplash.com/photo-1560759995-d237fb70a79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1866&q=60");
   background-position: center center;
