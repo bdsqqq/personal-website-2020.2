566d625e2ed657fb8b7fe4e75a07751f5f366f3d
Igor Bedesqui <igorbedesqui@gmail.com>
2020-09-09 17:15:35 -0300

    updated Hero

--- src/components/HeroWithHeader/index.tsx
+++ src/components/HeroWithHeader/index.tsx
@@ -4,9 +4,9 @@ import useToggleIsOpen from "../../hooks/useToggleState";
 import Header from "../Header";
 import Menu from "../Menu";
 
-import { HeroWrapper } from "./styles";
+import { HeroWrapper, PageTitleWrapper, PageTitle } from "./styles";
 
-const Hero = ({ children, ...rest }: any) => {
+const Hero = ({ pageTitle, children, ...rest }: any) => {
   const [menuIsOpen, setmenuIsOpen] = useState<boolean>(false);
 
   const ToggleMenu = () => {
@@ -16,6 +16,11 @@ const Hero = ({ children, ...rest }: any) => {
     <HeroWrapper {...rest}>
       <Header color="light" isOpen={menuIsOpen} toggleIsOpen={ToggleMenu} />
       <Menu isOpen={menuIsOpen} hide={ToggleMenu} />
+      {pageTitle && (
+        <PageTitleWrapper>
+          <PageTitle>{pageTitle}</PageTitle>
+        </PageTitleWrapper>
+      )}
       {children}
     </HeroWrapper>
   );
--- src/components/HeroWithHeader/styles.tsx
+++ src/components/HeroWithHeader/styles.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { mq } from "../../styles/global";
 
 const Div = ({ sm, ...rest }: any) => {
   return <div {...sm} {...rest}></div>;
@@ -9,10 +10,29 @@ export const HeroWrapper = styled(Div)`
   position: relative;
   width: 100%;
   height: ${(props) => (props.sm ? "30vh" : "100vh")};
-
+  min-height: 48rem;
   padding: 2.4rem;
   background-image: url("https://images.unsplash.com/photo-1560759995-d237fb70a79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1866&q=60");
   background-position: center center;
   background-repeat: no-repeat;
   background-size: cover;
+`;
+
+export const PageTitleWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const PageTitle = styled.h1`
+  font-size: 6.4rem;
+  letter-spacing: 0.2em;
+  margin-bottom: 2.4rem;
+  color: ${(props) => props.theme.colors.primaryWhite};
+
+  ${mq[0]} {
+    font-size: 8.8rem;
+  }
 `;
