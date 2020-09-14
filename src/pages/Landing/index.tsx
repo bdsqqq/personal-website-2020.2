b496417c5735722336cfc0a942d89d891183c7dc
bdsqqq <igorbedesqui@gmail.com>
2020-09-14 15:30:36 -0300

    added Folio to Landing

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -4,7 +4,7 @@ import {
   Page,
   ContentSection,
   SectionTitle,
-  SectionText,
+  SectionSubTitle,
   Footer,
 } from "../../styles/general";
 
@@ -19,6 +19,7 @@ import {
   StyledInseptionSVG,
 } from "./styles";
 
+import MasonryGrid from "../../components/MasonryGrid";
 import HeroWrapper from "../../components/HeroWithHeader";
 import SocialIcons from "../../components/SocialIcons";
 
@@ -43,23 +44,12 @@ const Landing = () => {
       </HeroWrapper>
 
       <ContentSection>
-        <SectionTitle>Lorem, ipsum dolor.</SectionTitle>
-        <SectionText>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-          eum voluptatem illo esse repellendus quas libero omnis, repellat quis
-          odio placeat quia, culpa error optio a est saepe sapiente itaque
-          tempora debitis officia atque. Mollitia aliquam distinctio quasi iure
-          tempora consequatur rerum laudantium, molestiae, atque, culpa optio
-          nostrum? Tenetur ipsum consequatur tempore accusamus explicabo
-          incidunt culpa qui eligendi repudiandae fugit ipsa minima enim
-          laudantium perspiciatis dolore laboriosam cum necessitatibus aperiam,
-          odit molestias non voluptates cupiditate labore. Dolor omnis veniam
-          sapiente.
-          <br /> <br /> <br />
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat
-          cumque ipsa fuga magnam excepturi velit, tempora laborum natus!
-          Officiis, dolores.
-        </SectionText>
+        <SectionTitle>Folio</SectionTitle>
+        <SectionSubTitle>
+          Clique em uma imagem para revelar ou ocultar detalhes sobre o
+          projeto...
+        </SectionSubTitle>
+        <MasonryGrid />
       </ContentSection>
 
       <Footer>
