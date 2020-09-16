def8e58e473874047349948d4c42da6b7ce85d4f
bdsqqq <igorbedesqui@gmail.com>
2020-09-16 18:57:47 -0300

    Removed Menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,40 +1,18 @@
-import React, { useState } from "react";
-import { FiMenu, FiX } from "react-icons/fi";
+import React from "react";
 
 import Logo from "../../components/Logo";
-
-import useToggleIsOpen from "../../hooks/useToggleState";
-
-import { HeaderWrapper, MenuIcon } from "./styles";
+import { HeaderWrapper } from "./styles";
 
 interface HeaderProps {
   color?: "light" | "dark";
-  isOpen: boolean;
-  toggleIsOpen: any;
 }
 
-const Header: React.FC<HeaderProps> = ({ color, isOpen, toggleIsOpen }) => {
+const Header: React.FC<HeaderProps> = ({ color }) => {
   return (
     <HeaderWrapper color={color}>
       <Logo color={color} />
-
-      <MenuIcon>
-        {isOpen ? (
-          <FiX aria-label="Fechar Menu" onClick={toggleIsOpen} />
-        ) : (
-          <FiMenu aria-label="Abrir Menu" onClick={toggleIsOpen} />
-        )}
-      </MenuIcon>
     </HeaderWrapper>
   );
 };
 
 export default Header;
-
-export const useHeaderMenu = (): [boolean, () => void] => {
-  const [menuIsOpen, setmenuIsOpen] = useState<boolean>(false);
-  const ToggleMenu = () => {
-    useToggleIsOpen(menuIsOpen, setmenuIsOpen);
-  };
-  return [menuIsOpen, ToggleMenu];
-};
